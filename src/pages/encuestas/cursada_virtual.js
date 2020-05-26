0fb8c6e8c7a5b6b363795b84484b98a3cef59b28
Matias Onorato <onoratomatias@gmail.com>
2020-05-26 15:28:59 -0300

    titulo encusta og

--- src/pages/encuestas/cursada_virtual.js
+++ src/pages/encuestas/cursada_virtual.js
@@ -15,8 +15,8 @@ export default function ({ encuesta }) {
         <Layout>
             <Head>
                 <title key="title">CEI UBA - Encuesta Cursada Virtual 1C 2020</title>
-                <meta property="og:title" key="og:title" content="Encuesta Cursada Virtual FIUBA 1C 2020" />
-                <meta property="og:description" key="og:description" content="Completá la encuesta para ayudarnos a analizar como está funcionando la cursada virtual en Fiuba" />
+                <meta property="og:title" key="og:title" content="Mejoremos la cursada virtual entre todos" />
+                <meta property="og:description" key="og:description" content="Entrá y completá la encuesta para ayudarnos a analizar como está funcionando la cursada virtual en Fiuba" />
                 <meta property="og:url" key="og:url" content="https://cei.fi.uba.ar/encuestas/cursada_virtual" />
                 <meta property="og:image" key="og:image" content="https://cei.fi.uba.ar/img/thum-encuesta-cursada-virtual-2.jpg" />
             </Head>
